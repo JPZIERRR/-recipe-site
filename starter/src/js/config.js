ccdc3253b2be029faa8af6fe57c71611dc5123c8
JPZIERRR <joaofornaz_ier@hotmail.com>
2023-05-03 11:35:01 -0300

    initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '87ec1253-74a0-41fb-b998-5f25045d6a2b';
+export const MODAL_CLOSE_SEC = 2.5;
